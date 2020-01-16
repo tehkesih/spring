8a8766fbbd1f84bad40abad0451fc9672c896df2
tehkesih <tehkesih.windy@gmail.com>
2020-01-16 11:57:37 +0800

    Taco Tuesday

---
+++ taco-cloud/src/main/java/com/tehkesih/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.tehkesih.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tehkesih.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+	
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		model.addAttribute("order", new Order());
+		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder (Order order) {
+		log.info("Order Submitted : " + order);
+		return "redirect:/";
+	}
+}
